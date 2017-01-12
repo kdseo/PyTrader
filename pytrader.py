21c2231c48c5ca1c11d8397b38f968ae5ef5940d
kdseo <namichelan@naver.com>
2017-01-13 02:24:59 +0900

    간단한 ui 테스트

--- pytrader.py
+++ pytrader.py
@@ -1,6 +1,8 @@
 import sys
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import QTimer
 from PyQt5 import uic
+import Kiwoom
 
 
 class MyWindow(object):
@@ -9,6 +11,16 @@ class MyWindow(object):
         super().__init__()
         self.ui = uic.loadUi("pytrader.ui")
         self.ui.show()
+
+        self.kiwoom = Kiwoom()
+        self.kiwoom.commConnect()
+
+        self.timer = QTimer(self)
+        self.timer.start(1000)
+        self.timer.timeout.connect(self.timeout)
+
+    def timeout(self):
+        self.ui.statusbar.showMessage(self.kiwoom.getConnectState())
 
 
 if __name__ == "__main__":

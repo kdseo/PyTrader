d8a6348a769584d53739262c34ca22de20db4a0d
kdseo <namichelan@naver.com>
2017-01-17 22:58:48 +0900

    ReturnCode 클래스 작성
    Kiwoom OpenApi+에서 반환하는 리턴코드를 클래스로 구성

--- Kiwoom.py
+++ Kiwoom.py
@@ -347,6 +347,40 @@ class ParameterTypeError(Exception):
         return self.msg
 
 
+class ReturnCode(object):
+
+    OP_ERR_NONE = 0 # 정상처리
+    OP_ERR_FAIL = -10   # 실패
+    OP_ERR_LOGIN = -100 # 사용자정보교환실패
+    OP_ERR_CONNECT = -101   # 서버접속실패
+    OP_ERR_VERSION = -102   # 버전처리실패
+    OP_ERR_FIREWALL = -103  # 개인방화벽실패
+    OP_ERR_MEMORY = -104    # 메모리보호실패
+    OP_ERR_INPUT = -105 # 함수입력값오류
+    OP_ERR_SOCKET_CLOSED = -106 # 통신연결종류
+    OP_ERR_SISE_OVERFLOW = -200 # 시세조회과부하
+    OP_ERR_RQ_STRUCT_FAIL = -201    # 전문작성초기화실패
+    OP_ERR_RQ_STRING_FAIL = -202    # 전문작성입력값오류
+    OP_ERR_NO_DATA = -203   # 데이터없음
+    OP_ERR_OVER_MAX_DATA = -204 # 조회가능한종목수초과
+    OP_ERR_DATA_RCV_FAIL = -205 # 데이터수신실패
+    OP_ERR_OVER_MAX_FID = -206  # 죄회가능한FID수초과
+    OP_ERR_REAL_CANCEL = -207   # 실시간해제오류
+    OP_ERR_ORD_WRONG_INPUT = -300   # 입력값오류
+    OP_ERR_ORD_WRONG_ACCTNO = -301  # 계좌비밀번호없음
+    OP_ERR_OTHER_ACC_USE = -302 # 타인계좌사용오류
+    OP_ERR_MIS_2BILL_EXC = -303 # 주문가격이20억원을초과
+    OP_ERR_MIS_5BILL_EXC = -304 # 주문가격이50억원을초과
+    OP_ERR_MIS_1PER_EXC = -305  # 주문수량이총발행주수의1%초과오류
+    OP_ERR_MIS_3PER_EXC = -306  # 주문수량이총발행주수의3%초과오류
+    OP_ERR_SEND_FAIL = -307 # 주문전송실패
+    OP_ERR_ORD_OVERFLOW = -308  # 주문전송과부하
+    OP_ERR_MIS_300CNT_EXC = -309    # 주문수량300계약초과
+    OP_ERR_MIS_500CNT_EXC = -310    # 주문수량500계약초과
+    OP_ERR_ORD_WRONG_ACCTINFO = -340    # 계좌정보없음
+    OP_ERR_ORD_SYMCODE_EMPTY = -500 # 종목코드없음
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
